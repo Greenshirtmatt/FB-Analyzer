fc434ab0041167de56238e2209dc98bda475443a
Matthew Sullivan <Greenshirtmatt@users.noreply.github.com>
2025-03-27 20:30:26 -1000

    three

---
+++ build/precache-manifest.7a6f8e979012442de6b19e656df1c5d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780792db32b68cda1d547f2291691ed3",
+    "url": "/facebook-data-analyzer/index.html"
+  },
+  {
+    "revision": "89d5f45226647b4cc80e",
+    "url": "/facebook-data-analyzer/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/facebook-data-analyzer/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "89d5f45226647b4cc80e",
+    "url": "/facebook-data-analyzer/static/js/main.21dc1a94.chunk.js"
+  },
+  {
+    "revision": "bee1d799997df68868d1",
+    "url": "/facebook-data-analyzer/static/js/runtime~main.690ccb2d.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Facebook Data Analyzer</h1>
+        <p>Welcome to the Facebook Data Analyzer app.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
